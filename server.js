126f1b9cffeb320920dd995c2a6feeff5015bcdb
Avan Gupta <96600087+avangupta@users.noreply.github.com>
2026-02-15 18:11:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const checkCorrect = require("./checkCorrect");
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 /* ---------- GLOBAL STATE ---------- */
 
