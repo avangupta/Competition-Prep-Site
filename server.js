fc05b21cb16ef3887658187665cf8727b423aca1
Avan Gupta <96600087+avangupta@users.noreply.github.com>
2026-02-15 18:47:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const checkCorrect = require("./checkCorrect");
 
 
 const app = express();
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 3000;
 
 /* ---------- GLOBAL STATE ---------- */
